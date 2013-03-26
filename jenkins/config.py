936ddde611b40eeef52c158c0c1a38e3c67d14cf
Andres Riancho <andres.riancho@gmail.com>
2013-03-26 12:26:28 -0300

    Adding joe's text editor

--- jenkins/config.py
+++ jenkins/config.py
@@ -30,7 +30,7 @@ env.conf = {
              
             'packages': ['python-virtualenv', 'git', 'python-pip', 'python2.7-dev',
                          'python-setuptools', 'build-essential', 'libsqlite3-dev',
-                         'libxml2-dev', 'libxslt-dev', 'apache2'],
+                         'libxml2-dev', 'libxslt-dev', 'apache2', 'joe'],
             
             'deploy_dir': '/opt/ci.w3af.org/w3af-qa/',
             'deploy_dir_parent': '/opt/ci.w3af.org/',
