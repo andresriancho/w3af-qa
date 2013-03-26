d311d570a62cd2732210bbd647f23f5235e9539d
Andres Riancho <andres.riancho@gmail.com>
2013-03-26 12:00:17 -0300

    Ignore if the file does not exist

--- jenkins/utils/filesystem.py
+++ jenkins/utils/filesystem.py
@@ -2,6 +2,6 @@ from __future__ import with_statement
 from fabric.api import sudo, env
 
 def links_to(orig, dest):
-    sudo('rm %s' % orig)
+    sudo('rm -rf %s' % orig)
     sudo('ln -s %s %s' % (dest, orig))
 
