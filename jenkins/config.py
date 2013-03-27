c6d3605269fcd210d3a7621fad758af001cae1b1
Andres Riancho <andres.riancho@gmail.com>
2013-03-27 13:39:03 -0300

    Making sure we have a mail server

--- jenkins/config.py
+++ jenkins/config.py
@@ -30,7 +30,8 @@ env.conf = {
              
             'packages': ['python-virtualenv', 'git', 'python-pip', 'python2.7-dev',
                          'python-setuptools', 'build-essential', 'libsqlite3-dev',
-                         'libxml2-dev', 'libxslt-dev', 'apache2', 'joe'],
+                         'libxml2-dev', 'libxslt-dev', 'apache2', 'joe',
+                         'postfix'],
             
             'deploy_dir': '/opt/ci.w3af.org/w3af-qa/',
             'deploy_dir_parent': '/opt/ci.w3af.org/',
